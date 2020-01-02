06e4736a0b52f068852ee7798430395efff9c5db
Luis Fernando Benavides Rengifo <33078898+Luisfben@users.noreply.github.com>
2020-01-02 17:08:08 -0500

    retiro bug major

--- src/main/java/com/hackerrank/challenges/impl/CurrencyFormatterServiceImpl.java
+++ src/main/java/com/hackerrank/challenges/impl/CurrencyFormatterServiceImpl.java
@@ -25,17 +25,14 @@ public class CurrencyFormatterServiceImpl implements CurrencyFormatterService {
 			throw new PaymentValueNotAllowedException(String.valueOf(p));
 		}
 
-		if (!(p >= 0 && p <= 1e9)) {
-			throw new PaymentValueNotAllowedException(String.valueOf(p));
-		}
-
+		/*
 		AtomicInteger aInt1 = new AtomicInteger(0);
 		AtomicInteger aInt2 = new AtomicInteger(0);
 
 		if (aInt1.equals(aInt2)) { 
 			System.out.println("Noncompliant");
 		}  
-		
+		*/
 		Locale indiaLocale = new Locale("en", "IN");
 
 		NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);

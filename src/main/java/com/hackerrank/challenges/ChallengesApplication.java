51b1d0ff7bc10c48011699b86a23aa9aaf9b4663
Luis Fernando Benavides Rengifo <33078898+Luisfben@users.noreply.github.com>
2020-01-05 18:16:25 -0500

    crear war

--- src/main/java/com/hackerrank/challenges/ChallengesApplication.java
+++ src/main/java/com/hackerrank/challenges/ChallengesApplication.java
@@ -2,14 +2,15 @@ package com.hackerrank.challenges;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.hackerrank.challenges"})
-public class ChallengesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ChallengesApplication.class, args);
+@ComponentScan({ "com.hackerrank.challenges" })
+public class ChallengesApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ChallengesApplication.class);
 	}
-
 }
